a89da85a1707fc41cc0dee512334fa552cb3aba7
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-09-22 16:06:20 +0900

    fix timezone?

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"strconv"
+	"time"
 	"unfire/config"
 	"unfire/route"
 )
+
+const location = "Asia/Tokyo"
+
+// fix time
+func init() {
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 9*60*60)
+	}
+	time.Local = loc
+}
 
 func main() {
 	cfg := config.GetInstance()
